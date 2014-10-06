cadd24bc6d8eda10e8030fc5b4692c4274486853
Ian Murdock <imurdock@exacttarget.com>
2014-10-06 17:53:15 -0400

    Deprecated in favor of string based filters.

---
+++ src/main-deprecated/java/com/exacttarget/fuelsdk/filter/ETFilterOperators.java
@@ -0,0 +1,71 @@
+//
+// This file is part of the Fuel Java SDK.
+//
+// Copyright (C) 2013, 2014 ExactTarget, Inc.
+// All rights reserved.
+//
+// Permission is hereby granted, free of charge, to any person
+// obtaining a copy of this software and associated documentation
+// files (the "Software"), to deal in the Software without restriction,
+// including without limitation the rights to use, copy, modify,
+// merge, publish, distribute, sublicense, and/or sell copies
+// of the Software, and to permit persons to whom the Software
+// is furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be
+// included in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY
+// KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
+// WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
+// PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS
+// OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES
+// OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT
+// OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH
+// THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+//
+
+package com.exacttarget.fuelsdk.filter;
+
+public enum ETFilterOperators {
+    EQUALS("equals"),
+    NOT_EQUALS("notEquals"),
+    LESS_THAN("lessThan"),
+    LESS_THAN_OR_EQUAL("lessThanOrEqual"),
+    GREATER_THAN("greaterThan"),
+    GREATER_THAN_OR_EQUAL("greaterThanOrEqual"),
+    IS_NULL("isNull"),
+    IS_NOT_NULL("isNotNull"),
+    BETWEEN("between"),
+    IN("IN"),
+    LIKE("like"),
+    BEGINS_WITH("beginsWith"),
+    CONTAINS("contains"),
+    ENDS_WITH("endsWith"),
+    EXISTS_IN_STRING("existsInString"),
+    EXISTS_IN_STRING_AS_A_WORD("existsInStringAsAWord"),
+    GREATER_THAN_ANNIVERSARY("greaterThanAnniversary"),
+    IS_ANNIVERSARY("isAnniversary"),
+    IS_NOT_ANNIVERSARY("isNotAnniversary"),
+    LESS_THAN_ANNIVERSARY("lessThanAnniversary"),
+    NOT_CONTAINS("notContains"),
+    NOT_EXISTS_IN_STRING("notExistsInString");
+    private final String value;
+
+    ETFilterOperators(String value) {
+        this.value = value;
+    }
+
+    public String value() {
+        return value;
+    }
+
+    public static ETFilterOperators fromValue(String value) {
+        for (ETFilterOperators v : ETFilterOperators.values()) {
+            if (v.value.equals(value)) {
+                return v;
+            }
+        }
+        throw new IllegalArgumentException(value);
+    }
+}
---
+++ src/main-deprecated/java/com/exacttarget/fuelsdk/filter/ETSimpleFilter.java
@@ -0,0 +1,109 @@
+//
+// This file is part of the Fuel Java SDK.
+//
+// Copyright (C) 2013, 2014 ExactTarget, Inc.
+// All rights reserved.
+//
+// Permission is hereby granted, free of charge, to any person
+// obtaining a copy of this software and associated documentation
+// files (the "Software"), to deal in the Software without restriction,
+// including without limitation the rights to use, copy, modify,
+// merge, publish, distribute, sublicense, and/or sell copies
+// of the Software, and to permit persons to whom the Software
+// is furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be
+// included in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY
+// KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
+// WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
+// PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS
+// OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES
+// OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT
+// OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH
+// THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+//
+
+package com.exacttarget.fuelsdk.filter;
+
+import java.util.Arrays;
+import java.util.Date;
+import java.util.List;
+
+public class ETSimpleFilter implements ETFilter {
+    private String property = null;
+    private ETFilterOperators operator = null;
+    private List<String> values = null;
+    private List<Date> dateValues = null;
+
+    public ETSimpleFilter() {}
+
+    @SuppressWarnings("unchecked")
+    public ETSimpleFilter(String property,
+                          ETFilterOperators operator,
+                          @SuppressWarnings("rawtypes") List values)
+    {
+        this.property = property;
+        this.operator = operator;
+
+        if (values.size() > 0) {
+            Object o = values.get(0);
+            if (o instanceof String) {
+                this.values = (List<String>) values;
+            } else if (o instanceof Date) {
+                this.dateValues = (List<Date>) values;
+            }
+        }
+    }
+
+    public ETSimpleFilter(String property,
+                          ETFilterOperators operator,
+                          String value)
+    {
+        this.property = property;
+        this.operator = operator;
+        this.values = Arrays.asList(value);
+    }
+
+    public ETSimpleFilter(String property,
+                          ETFilterOperators operator,
+                          Date value)
+    {
+        this.property = property;
+        this.operator = operator;
+        this.dateValues = Arrays.asList(value);
+    }
+
+    public String getProperty() {
+        return property;
+    }
+
+    public void setProperty(String property) {
+        this.property = property;
+    }
+
+    public ETFilterOperators getOperator() {
+        return operator;
+    }
+
+    public void setOperator(ETFilterOperators operator) {
+        this.operator = operator;
+    }
+
+    public List<String> getValues() {
+        return values;
+    }
+
+    public void setValues(List<String> values) {
+        this.values = values;
+    }
+
+    public List<Date> getDateValues() {
+        return dateValues;
+    }
+
+    public void setDateValues(List<Date> dateValues) {
+        this.dateValues = dateValues;
+    }
+}
