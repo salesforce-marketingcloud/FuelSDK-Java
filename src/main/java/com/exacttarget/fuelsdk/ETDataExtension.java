fd5641d66ba83c023325fa2a6edfac8df7f214b9
Ian Murdock <imurdock@exacttarget.com>
2015-01-28 21:47:12 -0500

    removed unused import directive

--- src/main/java/com/exacttarget/fuelsdk/ETDataExtension.java
+++ src/main/java/com/exacttarget/fuelsdk/ETDataExtension.java
@@ -33,7 +33,6 @@ import java.util.List;
 import java.util.Map;
 
 import com.google.gson.Gson;
-import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
